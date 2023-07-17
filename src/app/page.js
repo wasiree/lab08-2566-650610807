2a75d1a4c9b9d007eccad266da85c89bef1fc2f5
wasiree <wasiree098@gmail.com>
2023-07-17 16:14:14 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
       {/* //footer section */}
       <div className="mt-3">
         <p className="text-secondary text-center">
-          copyright © 2023 Chayanin Suatap 12345678
+          copyright © 2023 Vasiree Nuntajan 650610807
         </p>
       </div>
     </div>
